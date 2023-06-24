853a8eb18fdfdf58a629479752393df8097d7856
Starikov_M <Starikov_M@student.itstep.org>
2023-06-24 13:59:28 +0300

    programm

--- WinFormsApp5/Form1.Designer.cs
+++ WinFormsApp5/Form1.Designer.cs
@@ -29,20 +29,46 @@ namespace WinFormsApp5
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(601, 365);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter date";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(617, 468);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1443, 905);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WinFormsApp5/Form1.cs
+++ WinFormsApp5/Form1.cs
@@ -17,5 +17,25 @@ namespace WinFormsApp5
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 10)
+            {
+                MessageBox.Show("Wrong input(length)","Error date" ,MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if(textBox1.Text.Count(l => l == '.') != 2)
+            {
+                MessageBox.Show("Wrong input(got error)", "Error date", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (textBox1.Text.Any(l => char.IsLetter(l)))
+            {
+                MessageBox.Show("Wrong input(has letters)", "Error date", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            DateTime date = Convert.ToDateTime(textBox1.Text);
+            MessageBox.Show($"{date.DayOfWeek}");
+        }
     }
 }
